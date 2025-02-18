7ede21394b3aa06cab2151ce8137a1a0cbc105cb
Ahammad Ali Pinjari <ahammad0814@gmail.com>
2025-02-18 13:36:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function hover(params) {
+	const ele1 = document.querySelector(`#square${params == 1 ? 2 : 1}`);
+	const ele2 = document.querySelector(`#square${(params == 1 || params == 2) ? 3 : 2}`);
+
+	ele1.style.background = "#6F4E37";
+	ele2.style.background = "#6F4E37";
+}
+
+function leaved() {
+	document.querySelector("#square1").style.background = "#E6E6FA";
+	document.querySelector("#square2").style.background = "#E6E6FA";
+	document.querySelector("#square3").style.background = "#E6E6FA";
+}
